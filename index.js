8c769b47e159b598ac49ded9fe80875a7f8619ff
Allen <allencpsc@gmail.com>
2023-09-15 12:25:43 -0400

    files for node lecture 4

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser")
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 app.use('/static', express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -21,7 +24,13 @@ app.get('/query', function(req, res){
     res.render('home.ejs', nameObject);
 })
 
+app.post('/create', (req, res) => {
+    console.log(req.body)
+    //make sure app doesnt get stuck on create page
+    res.redirect('/')
+})
 
+// put/post need body, get/delete does not 
 app.listen(3000, () => {
     console.log('started on port 3000');
 })
